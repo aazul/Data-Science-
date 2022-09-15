c9240847ff4d2fbb9ce3077186aec41c0d35eee0
Azul Carrillo <atziri@ucmerced-10-34-65-6.ucmerced.edu>
2022-09-15 13:06:05 -0700

    This is the first part of the tropical plant trait

---
+++ tropical-plant-trait.R
@@ -0,0 +1,29 @@
+# setting the working directory where the file is
+#Reading the csv 
+read.csv("Traitplantdatafordryad.csv")
+
+getwd()
+
+unique(x=)
+#option dash <- 
+datatable <- 
+  datatable <- read.csv("Traitplantdatafordryad.csv")
+# explore the head of datatable 
+head(datatable)
+#to see all the rows you leave the first before the commma empty 
+datatable[,3]
+# gives us the unique family found 
+unique(x= datatable[,3])
+
+uniquefamilies <- unique(x= datatable[,3])
+
+length(uniquefamilies) #there are 44 species in each family 
+
+datatable[,1] #this will give me the species column 
+
+unique(x=datatable[,1]) 
+
+length(datatable)
+length(datatable[,1])
+length(unique(datatable[,1]) ) #putting two commands into 1 
+
